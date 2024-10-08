18008bb6378d6fcbe284be3653e40e3a5d9ad98a
coltenthefirst <142692930+coltenthefirst@users.noreply.github.com>
2024-10-08 02:33:57 -0500

    Update high.py

--- high.py
+++ high.py
@@ -1,39 +1,28 @@
 import os
 from PIL import Image
 
-# /Users/coltenparker/Downloads/Image-to-roblox/high.py
 with open("/Users/coltenparker/Downloads/Image-to-roblox/output/output_file.txt", "w") as f:
     f.write("High processing result")
 
-factor = 3.2  # Rate of change to resolution 1: Original resolution. ie: 640x640 - 6.4 - 100x100
-rate = 50    # Rate that parts are created at
+factor = 3.2
+rate = 50
 
 for filename in os.listdir("input"):
     image_path = os.path.join("input", filename)
     if os.path.isfile(image_path):
         image = Image.open(image_path)
-
-        # Resize image
         image = image.resize((int(image.size[0] / factor), int(image.size[1] / factor)))
         pixels = image.load()
-
-        # Prepare output filename without extension
         output_filename = os.path.splitext(filename)[0]
 
-        # Write Lua file
         with open(f"{os.path.join('output', output_filename)}.lua", 'w') as f:
-
             bits = []
-
-            # Iterate through pixels correctly
-            for y in range(image.size[1]):  # Height
-                for x in range(image.size[0]):  # Width
+            for y in range(image.size[1]):
+                for x in range(image.size[0]):
                     p = pixels[x, y]
                     p = ("{:03d}".format(p[0]), "{:03d}".format(p[1]), "{:03d}".format(p[2]))
-
                     bits.append(''.join(map(str, p)))
 
-            # Write the Lua script content
             f.write("require(script.Parent.Parent):Draw(" + str(rate) + 
                     ", Vector3.new(0,0,0), {" + str(image.size[0]) + "," + str(image.size[1]) + 
                     "}, '" + ''.join(bits) + "')")
