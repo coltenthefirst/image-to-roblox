bac8c69ad33651415ec0bd9525c3c63e31dbb4fe
Unknowingly_Exists <142692930+coltenthefirst@users.noreply.github.com>
2024-11-06 12:50:07 -0600

    Update low.py

--- low.py
+++ low.py
@@ -3,40 +3,27 @@ from PIL import Image
 
 factor = 12.8
 rate = 200
-input_directory = "/tmp/input"
-output_directory = "/tmp/output"
-lua_file_path = os.path.join(output_directory, "output.lua")
 
-os.makedirs(output_directory, exist_ok=True)
-
-all_bits = []
-
-with open(lua_file_path, 'w') as f:
-    f.write("")
-
-for filename in os.listdir(input_directory):
-    image_path = os.path.join(input_directory, filename)
+for filename in os.listdir("/tmp/input"):
+    image_path = os.path.join("/tmp/input", filename)
     if os.path.isfile(image_path):
-        try:
-            image = Image.open(image_path)
-            image = image.resize((int(image.size[0] / factor), int(image.size[1] / factor)))
-            pixels = image.load()
+        image = Image.open(image_path)
+        image = image.resize((int(image.size[0] / factor), int(image.size[1] / factor)))
+        pixels = image.load()
+        output_filename = os.path.splitext(filename)[0]
 
+        with open(f"{os.path.join('/tmp/output', output_filename)}.lua", 'w') as f:
+            bits = []
             for y in range(image.size[1]):
                 for x in range(image.size[0]):
                     p = pixels[x, y]
                     p = ("{:03d}".format(p[0]), "{:03d}".format(p[1]), "{:03d}".format(p[2]))
-                    all_bits.append(''.join(map(str, p)))
+                    bits.append(''.join(map(str, p)))
 
-            print(f"Processed: {filename}")
+            f.write("require(script.Parent.Parent):Draw(" + str(rate) + 
+                    ", Vector3.new(0,0,0), {" + str(image.size[0]) + "," + str(image.size[1]) + 
+                    "}, '" + ''.join(bits) + "')")
 
-        except Exception as e:
-            print(f"Error processing {filename}: {e}")
+        print(f"Processed: {filename}")
 
-with open(lua_file_path, 'w') as f:
-    f.write("require(script.Parent.Parent):Draw(" + str(rate) + 
-            ", Vector3.new(0,0,0), {" + str(image.size[0]) + "," + str(image.size[1]) + 
-            "}, '" + ''.join(all_bits) + "')")
-
-print(f"Wrote to Lua file: {lua_file_path}")
 print("Done!")
