f4db224d77b0a11fcc894b5a086751e1a15d7f46
Unknowingly_Exists <142692930+coltenthefirst@users.noreply.github.com>
2024-11-06 21:55:03 -0600

    Update server.py

--- api/server.py
+++ api/server.py
@@ -96,4 +96,4 @@ def send_image():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
